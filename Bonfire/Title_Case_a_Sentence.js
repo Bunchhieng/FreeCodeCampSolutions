b8601214a00edab3a59d4a2af511bc7c547926e6
U-MUTUALINK\bsoth <bsoth@BSOTH01.corp.mutualink.net>
2015-09-28 13:01:07 -0400

    Added more Bonfire

---
+++ Bonfire/Title_Case_a_Sentence.js
@@ -0,0 +1,14 @@
+function titleCase(str) {
+    str = str.toLowerCase().split("");
+    str[0] = str[0].toUpperCase();
+    for (i = 1; i < str.length; i++) {
+        if (str[i + 1] == " ") {
+            str[i + 2] = str[i + 2].toUpperCase();
+        }
+    }
+
+    str = str.join("");
+    return str;
+}
+
+console.log(titleCase("I'm a little tea pot", ""));
