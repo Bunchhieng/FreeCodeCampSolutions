2fd6ab0e2f3abc2dcfa26d331fd480ed331a7f69
Bunchhieng <bunchhieng@gmail.com>
2016-01-08 21:08:56 -0500

    fix .gitignore

--- Bonfire/Check_for_Palindromes.js
+++
@@ -1,7 +0,0 @@
-function palindrome(str) {
-  // Good luck!
-  str = str.replace(/[^A-Za-z]/g, "").toLowerCase();
-  return str === str.split("").reverse().join("");
-}
-
-palindrome("eye");
--- Bonfire/Falsy_Bouncer.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Bunchhieng on 1/8/2016.
- */
-function bouncer(arr) {
-    return arr.filter(Boolean);
-}
-
-console.log(bouncer([7, "ate", "", false, 9]));
-console.log(bouncer([false, null, 0, NaN, undefined, ""]));
